910bf13588bc49917ed4f07eaf064aac4db64463
tho do <thodo7199@gmail.com>
2021-04-18 10:06:05 +0700

    push-repository

---
+++ src/App.js
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from "react";
+import { useCurrentPosition, useWatchPosition } from 'react-use-geolocation';
+import "./App.css";
+import BgWeather from "./components/bgWeather";
+import DetailsWeather from "./components/DetailWeather";
+
+import Clear from "./images/Atmosphere.jpg";
+import Atmosphere from "./images/Clear.jpg";
+import Clouds from "./images/Clouds.jpg";
+import Drizzle from "./images/Drizzle.jpg";
+import Rain from "./images/Rain.jpg";
+import Snow from "./images/Snow.jpg";
+import Thunderstorm from "./images/Thunderstorm.jpg";
+const App = () => {
+  
+ 
+  const [weather, setWeather] = useState(null);
+  const [position,error] = useWatchPosition()
+  const [latLng, setLatLog] = useState({lat:0,lng:0});
+ 
+  const callHandleLatLng = latLng => {
+    setLatLog(latLng);
+  };
+  console.log(latLng);
+  // useEffect(()=>{
+  //   setLatLog({
+  //    lat:position.coords.latitude,
+  //    lng:position.coords.longitude
+
+  //   })
+  // },[])
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+      //   console.log(latLng.lat)
+      //   if(position.coords!==null){
+      //  setLatLog({
+      //     lat:position.coords.latitude,
+      //     lng:position.coords.longitude
+      //   })
+      //     console.log(latLng.lat)
+      //   }
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latLng.lat}&lon=${latLng.lng}&appid=e1a5f0e6dbe87d1067ada6dd9a2ee751`;
+        const res = await fetch(url);
+        const response = await res.json();
+
+        setWeather(response);
+      } catch (error) {}
+    }
+    fetchWeather();
+  }, [latLng,position]);
+
+  
+  if(weather!==null){
+    const code = weather.weather[0].id;
+    console.log(code);
+    function desc() {
+      let description;
+  
+      if (code < 300) {
+        description = Thunderstorm;
+        return description;
+      } else if (code >= 300 && code < 321) {
+        description = Drizzle;
+        return description;
+  
+      } else if (code >= 500 && code < 600) {
+        description = Rain;
+        return description;
+  
+      } else if (code >= 600 && code < 700) {
+        description = Snow;
+        return description;
+  
+      } else if (code > 700 &&
+         code < 750) {
+        description = Clear;
+        return description;
+  
+      }
+      
+      else if (code===800) {
+        description = Atmosphere;
+        return description;
+  
+      }
+      else if (code > 800) {
+        description = Clouds;
+        return description;
+  
+      }
+      else{
+        return;
+      }
+      
+    }
+   var bgImg = desc()
+   console.log(bgImg)
+  }
+  
+
+  // if (!position && !error) {
+  //   return <p>Waiting...</p>;
+  // }
+ 
+  // if (error) {
+  //   return <p>{error.message}</p>;
+  // }
+ 
+
+  return (
+    <div className="app" style={{backgroundImage:`url(${bgImg})`}}>
+      <div className="app-weather">
+       
+        <div className="container wrapper-col">
+        {/* <CurrentLocation /> */}
+          <div className="row wrapper-col">
+            
+            <div className="col-lg-8 col-sm-12 wrapper-col bg" style={{backgroundImage:`url(${bgImg})`}}>
+              <BgWeather propsWeather={weather} />
+              </div>
+              <div className="col-lg-4 col-sm-12 details">
+              <DetailsWeather
+                handleLatLng={callHandleLatLng}
+                propsWeather={weather}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/DetailWeather/weather/index.js
@@ -0,0 +1,67 @@
+import usePlacesAutocomplete, { getGeocode,getLatLng} from "use-places-autocomplete";
+import "./weather.css"
+  
+  const PlacesAutocomplete = (props) => {
+    const{handleLatLng} = props;
+    const { ready,
+      value,
+      suggestions: { status, data }, 
+      setValue, 
+      clearSuggestions,} = usePlacesAutocomplete({
+      requestOptions: {
+        /* Define search scope here */
+      },
+      debounce: 300,
+    });
+    console.log(value)
+    const handleInput = (e) => {
+      // Update the keyword of the input element
+      setValue(e.target.value);
+    };
+  const handleReset=()=>{
+    setValue('')
+  }
+    const handleSelect = ({ description }) => () => {
+      // When user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+  
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description })
+        .then((results) => getLatLng(results[0]))
+        .then((latLng) => {
+          console.log("Coordinates: ",latLng);
+          handleLatLng(latLng)
+        })
+        .catch((error) => {
+          console.log("😱 Error: ", error);
+        });
+    };
+   
+    const renderSuggestions = () =>
+      data.map((suggestion) => {
+        const { place_id, structured_formatting: { main_text, secondary_text } } = suggestion;
+  
+        return (
+          <li className="location__item" key={place_id} onClick={handleSelect(suggestion)}>
+            <strong>{main_text}</strong> <small>{secondary_text}</small>
+          </li>
+        );
+      });
+  
+    return (
+      <div className="container autocomplete" >
+        
+        <input className="form-group"
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Where are you going?"
+        />
+        {status === "OK" && <ul className="location__list">{renderSuggestions()}</ul>}
+        <button className="btn-reset" onClick={handleReset}>Reset</button>
+      </div>
+    );
+  };
+  export default PlacesAutocomplete;
